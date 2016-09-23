748a7f6ae83848482772d6cea17d789b3e8f1a04
Joel Gilley <gilleyj@gmail.com>
2016-09-23 12:24:36 -0400

    Convox start with code sync

--- containers/ubuntu16.04-nginx-php7/README.md
+++ containers/ubuntu16.04-nginx-php7/README.md
@@ -6,3 +6,10 @@
 
 `docker build -t php:7.0 .`
 
+**RUN**
+
+interactively:
+
+`docker run -i -t --entrypoint /bin/bash php:7.0`
+
+
--- containers/ubuntu16.04-nginx-php7/start.sh
+++ containers/ubuntu16.04-nginx-php7/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Hello! V 0.10"
+echo "Hello! V 0.11"
 cat /etc/nginx/sites-availabe/default
 find /webroot/
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
