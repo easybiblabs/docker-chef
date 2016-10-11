b663b3adb56475b8ce9d2779f4513dc79faa19ce
Joel Gilley <gilleyj@gmail.com>
2016-10-11 10:53:41 -0400

    Removal of wpt code

--- convox-wpt/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo 'building configurations...'
-php build_config.php
-
-echo " starting supervisord ... "
-/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
-echo "Goodbye"
